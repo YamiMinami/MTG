44267aaf9e11f26685bf34386277527251c37af7
sean snel (s161664) <sean.snel@student.ap.be>
2025-03-05 10:38:35 +0000

    popup animatie

--- js/script.js
+++ js/script.js
@@ -20,5 +20,11 @@ document.addEventListener("DOMContentLoaded", function () {
       selectedAttraction: 0.01,
       friction: 0.25,
     });
-  });
+});
+let addButtondeck = document.getElementById("add-deck-buttons")
+
+addButtondeck.addEventListener("click", (e) => {
+  let popup = document.getElementById("add-to-deckcollection");
+  popup.classList.add("show");
+  })
   
