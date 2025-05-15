2b1e99911da46437c6429653932fdb4652881036
Sofiane Hammich <sofiane.hammich@hotmail.com>
2025-05-15 18:50:25 +0000

    filter functie interface veranderd

--- mtk/interfaces.ts
+++ mtk/interfaces.ts
@@ -1,11 +1,12 @@
 export interface Card {
-    id: string;                   
-    name: string;             
-    rarity: 'common' | 'uncommon' | 'rare' | 'mythic'; 
-    "color_identity": string[],
-    oracle_text?: string;         
-    image_uris?: {
-      png?: string;             
-    };
-  }
-  
+  id: string;
+  name: string;
+  rarity: "common" | "uncommon" | "rare" | "mythic";
+  color_identity?: {
+    0?: string[];
+  };
+  oracle_text?: string;
+  image_uris?: {
+    png?: string;
+  };
+}
