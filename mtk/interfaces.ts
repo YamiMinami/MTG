fc5554a88e4d1244b62b122d21f77fc4fcecdf6c
s117507 <s117507@ap.be>
2025-05-16 10:14:05 +0000

    updated flavor text

--- mtk/interfaces.ts
+++ mtk/interfaces.ts
@@ -2,6 +2,7 @@ export interface Card {
     id: string;                   
     name: string;             
     rarity: 'common' | 'uncommon' | 'rare' | 'mythic'; 
+    flavor_text?: string;
     oracle_text?: string;         
     image_uris?: {
       png?: string;             
